38ad9d9113bebe74b4a7d9590c50a407aee3b5ed
SANDEEP vaid <vaid77167@gmail.com>
2021-12-21 22:53:40 +0530

    modifying state

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,9 +10,43 @@ class CartIem extends React.Component{
         this.state = {
             price:99,
             title:'Realme Phone',
-            qty:1,
+            qty:0,
             img:""
         }
+    }
+
+    //Event Handlers function
+    increaseQuantity = ()=>{
+        //For changing the state we use inbuilt functionality called as setState and there are two way to implement it
+
+        //setState => It is an asynchronous call and also use batching concept but it only works when we use event handler means we dont know when that work is completed or not . To work with asnyc. call we can also use callback as second argument and also setState use the concept of batching which means if we make three calls from way1 it works only for last call by merging all calls in 1 call by shallow merging concept and rerender once but in 2nd call it works for all but rerender once .
+
+
+        //Way 1 
+        // this.setState({
+        //     qty:this.state.qty += 1
+        // });
+
+        //way2 in that way we use the previous value to update it
+        this.setState((prevstate)=>{
+            return {qty:prevstate.qty + 1}
+        },()=>{ //this is a callback function
+            console.log('completed async call',this.state.qty)
+        })
+    }
+
+    //Event handler for decrease quantity
+    decreaseQuantity = ()=>{
+
+        const {qty} = this.state;
+
+        if(qty != 0){
+            this.setState((prevstate)=>{
+                return {qty : prevstate.qty - 1}
+            })
+        }
+
+        
     }
 
     render(){
@@ -37,13 +71,14 @@ class CartIem extends React.Component{
                         </div >
 
                         <div className="lower-part">
-                            <button className="action-icons">
+                            {/*Here we use bind so that when we call that function we can access that object value or we can use arrow function*/}
+                            <button onClick={this.increaseQuantity} className="action-icons">
                                 +
                             </button>
                             <span className="action-icons">
                                 {qty}
                             </span>
-                            <button className="action-icons">
+                            <button onClick={this.decreaseQuantity} className="action-icons">
                                 -
                             </button>
                         </div>
